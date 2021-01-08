e18387db9ef02f1fa9561f44594d76bf8cb0cbb3
Ferran Montes <ferran.montes@dynatrace.com>
2021-01-08 17:47:26 +0100

    modified covid stuff

--- projects/covid19/covid19.py
+++ projects/covid19/covid19.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import os
+import seaborn as sns
 
 # data from https://github.com/CSSEGISandData/COVID-19
 file_confirmed = '/projects/covid19/data/time_series_covid19_confirmed_global.csv'
@@ -10,9 +11,14 @@ df_confirmed = df_confirmed.set_index('Country/Region')
 df_confirmed = df_confirmed.drop('Province/State', axis=1)
 df_confirmed = df_confirmed.drop('Lat', axis=1)
 df_confirmed = df_confirmed.drop('Long', axis=1)
+print(df_confirmed.info())
+print(df_confirmed.head())
+
+# Confirmed Cases Global by Date
 fig = plt.figure(figsize=(50,8), dpi=100)
-plt.plot(df_confirmed.loc['Afghanistan'])
-plt.plot(df_confirmed.loc['Spain'])
+#plt.plot(df_confirmed.loc['Afghanistan'])
+#plt.plot(df_confirmed.loc['Spain'])
+sns.barplot(data=df_confirmed, y=df_confirmed.index, x='1/2/21', orient='h')
 plt.xticks(rotation=90) # todo: specify columns as date data type
 plt.show()
 
