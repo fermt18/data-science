efef81b85ee5486698641e30bbbdf1182a156e4a
Ferran Montes <fermt18@gmail.com>
2021-01-09 17:39:52 +0100

    fixed covid graphs

--- projects/covid19/covid19.py
+++ projects/covid19/covid19.py
@@ -14,26 +14,49 @@ df_confirmed = df_confirmed.drop('Long', axis=1)
 print(df_confirmed.info())
 print(df_confirmed.head())
 
-# Confirmed Cases Global by Date
-N = len(df_confirmed.columns)
-fig = plt.figure(figsize=(8,10)) # blank canvas
+def confirmed_cases_global_by_date(date): #'1/2/21'
+    N = len(df_confirmed.columns)
+    fig = plt.figure(figsize=(8,10), dpi=150)
+    plt.title('Covid-19 Confirmed Cases on: ' + date)
+    ax = fig.gca()
+    sns.barplot(data=df_confirmed, x=df_confirmed.index, y=date, orient='v')
+    plt.xticks(range(N), rotation=90)
+    plt.grid()
+    ax.margins(x=0)
+    ax.set_yscale('log')
+    ax.set_xlabel('Countries')
+    ax.set_ylabel('Confirmed Cases')
+    fig.canvas.draw()
+    tl = ax.get_xticklabels()
+    maxsize = max([t.get_window_extent().width for t in tl])
+    m = 0.1 # inch margin
+    s = maxsize/fig.dpi*N+2*m
+    margin = m/fig.get_size_inches()[0]
+    fig.subplots_adjust(left=margin, right=1.-margin)
+    fig.set_size_inches(s, fig.get_size_inches()[0])
+    plt.savefig(__file__+".png")
+    plt.show()
+
+
+# Confirmed Cases Evolution
+fig = plt.figure(figsize=(25,10), dpi=150)
+plt.title('Covid-19 Confirmed Cases')
+#countries = df_confirmed.index
+countries = ['Spain', 'US', 'Germany', 'India', 'Turkey', 'Brazil']
+for country in countries:
+    plt.plot(df_confirmed.loc[country], label=country)
+plt.plot(df_confirmed.loc[country])
+plt.xticks(rotation=90)
 ax = fig.gca()
-sns.barplot(data=df_confirmed, y=df_confirmed.index, x='1/2/21', orient='h')
-plt.yticks(range(N), rotation=0) # add loads of ticks
-plt.grid()
-ax.margins(y=0)
-fig.canvas.draw()
-tl = ax.get_yticklabels()
-maxsize = max([t.get_window_extent().height for t in tl])
-m = 0.2 # inch margin
-s = maxsize/fig.dpi*N+2*m
-margin = m/fig.get_size_inches()[1]
-fig.subplots_adjust(left=margin, right=1.-margin)
-fig.set_size_inches(s, fig.get_size_inches()[0])
-#plt.savefig(__file__+".png")
+ax.set_xlabel('Dates')
+ax.set_ylabel('Confirmed Cases')
+ax.legend()
+locs, labels = plt.xticks()
+for l in labels[::1]:
+    l.set_visible(False)
+for l in labels[::20]:
+    l.set_visible(True)
 plt.show()
-
-
 
 file_deaths = '/projects/covid19/data/time_series_covid19_deaths_global.csv'
 df_deaths = pd.read_csv(os.getcwd() + file_deaths)
